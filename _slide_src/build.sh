3082f89c3958e0c5bb28e728c790cde70a91424c
Bryce Boe <bbzbryce@gmail.com>
2015-09-22 15:22:49 -0700

    Add and support custom css for landslide slides.

--- _slide_src/build.sh
+++ _slide_src/build.sh
@@ -3,18 +3,28 @@
 DESTINATION="../slides/"
 
 function process_file {
+    # Process only files with .md extension
     if [[ ${1: -3} != ".md" ]]; then
         echo "Ignoring $1 (does not have .md extension)"
         return
     fi
-    base=${1::${#1}-3}
-    landslide $1 -ro > "$DESTINATION/$base.html"
+
+    # Create the config.cfg file
+    sed "s/<SOURCE>/$1/" CONFIG_TEMPLATE.CFG > config.cfg
+
+    # Run landslide
+    landslide config.cfg > /dev/null
     if [ $? -eq 0 ]; then
         echo "Built $1"
     else
         echo "Failed to build $1. Aborting!"
+        rm -f config.cfg presentation.html
         exit 3
     fi
+
+    # Move the file to the appropriate locaation
+    base=${1::${#1}-3}
+    mv presentation.html "$DESTINATION/$base.html"
 }
 
 
@@ -42,5 +52,6 @@ fi
 for arg in "$@"; do
     process_file "$arg"
 done
+rm config.cfg
 
 exit 0;
